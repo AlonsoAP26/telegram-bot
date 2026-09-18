da771e227684fc4c418313df2d71d05fcda6ba42
AlonsoAP26 <alonso614289@gmail.com>
2026-09-18 16:40:26 -0500

    Actualiza tiempo de espera

--- bot.py
+++ bot.py
@@ -141,7 +141,7 @@ async def logUserBot():
                 parse_mode="HTML",
             )
 
-            await asyncio.sleep(240)
+            await asyncio.sleep(100)
 
         except Exception as error:
             print(f"Error: {error}")
